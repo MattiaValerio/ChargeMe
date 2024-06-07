087590bbf11d292f88c4f0bc4222b02ed2f25a23
Matteoeo <matteo.maurutto@gmail.com>
2024-06-07 10:45:27 +0200

    modifica tabelle

--- ChargeMe.Shared/Models/ChargingModel.cs
+++ ChargeMe.Shared/Models/ChargingModel.cs
@@ -5,6 +5,10 @@ public class Charging : Car
     public required ChargingStation Station { get; set; } 
     public CarStatus ChargingStatus { get; set; }
     public int ChargingPerc { get; set; }
+    public int ChargingPrice { get; set; }
+    public double Voltage { get; set; }
+    public TimeOnly LeastTime { get; set; }
+    
 }
 
 public enum CarStatus
