4561ddb6c6e15654aef81b53043492a3fb762bcb
Mattia <prodev@proconsulting.it>
2024-06-24 16:57:29 +0200

    chore: Update Vite configuration for frontend

--- chargeme.frontend/vite.config.ts
+++ chargeme.frontend/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   preview: {
@@ -19,6 +19,6 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     host: true,
-    origin: 'http://localhost:8080',
+    origin: "http://localhost:80",
   },
 });
