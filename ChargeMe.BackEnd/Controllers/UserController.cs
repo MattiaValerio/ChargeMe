344ca086d61d681451f7ac09cbd3ba86c4a1918f
Mattia <prodev@proconsulting.it>
2024-06-24 17:49:29 +0200

    m

--- ChargeMe.BackEnd/Controllers/UserController.cs
+++ ChargeMe.BackEnd/Controllers/UserController.cs
@@ -56,7 +56,7 @@ public class UserController : ControllerBase
     {
         try
         {
-            Console.WriteLine(json  );
+            Console.WriteLine(json);
 
             var userdto = json;
             
