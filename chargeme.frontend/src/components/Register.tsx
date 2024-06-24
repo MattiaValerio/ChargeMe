d0ac8955e884bfb392e17e85a4c6d2d894d99749
Mattia <prodev@proconsulting.it>
2024-06-24 16:51:26 +0200

    fetch person on registration

--- chargeme.frontend/src/components/Register.tsx
+++ chargeme.frontend/src/components/Register.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { RegisterSchema } from '@/types/RegisterSchema';
-import { z } from 'zod';
-import { Button } from '@/components/ui/button';
-import { Label } from '@radix-ui/react-label';
-import { Input } from '@/components/ui/input';
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/types/RegisterSchema";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
 
 const Register: React.FC = () => {
   const {
@@ -16,18 +16,37 @@ const Register: React.FC = () => {
   } = useForm<z.infer<typeof RegisterSchema>>({
     resolver: zodResolver(RegisterSchema),
     defaultValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      city: '',
-      address: '',
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      city: "",
+      address: "",
     },
   });
 
   const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
-    console.log(values);
+    // create an object with all the values and fetch it to the backend
+    const person = {
+      firstName: values.firstName,
+      lastName: values.lastName,
+      email: values.email,
+      password: values.password,
+      city: values.city,
+      address: values.address,
+    };
+
+    // fecth the object to the backend
+    fetch("chargeme.backend:5000/api/register", {
+      method: "POST",
+      body: JSON.stringify(person),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
   };
 
   return (
@@ -37,14 +56,14 @@ const Register: React.FC = () => {
           <div className="grid grid-cols-2 gap-4 mt-4">
             <div className="grid gap-2 ">
               <Label htmlFor="firstName">First name</Label>
-              <Input id="firstName" {...register('firstName')} />
+              <Input id="firstName" {...register("firstName")} />
               {errors.firstName && (
                 <p className="error-message">{errors.firstName.message}</p>
               )}
             </div>
             <div className="grid gap-2">
               <Label htmlFor="lastName">Last name</Label>
-              <Input id="lastName" {...register('lastName')} />
+              <Input id="lastName" {...register("lastName")} />
               {errors.lastName && (
                 <p className="error-message">{errors.lastName.message}</p>
               )}
@@ -52,14 +71,14 @@ const Register: React.FC = () => {
           </div>
           <div className="space-y-1">
             <Label htmlFor="email">E-mail</Label>
-            <Input id="email" type="email" {...register('email')} />
+            <Input id="email" type="email" {...register("email")} />
             {errors.email && (
               <p className="error-message">{errors.email.message}</p>
             )}
           </div>
           <div className="space-y-1">
             <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" {...register('password')} />
+            <Input id="password" type="password" {...register("password")} />
             {errors.password && (
               <p className="error-message">{errors.password.message}</p>
             )}
@@ -69,7 +88,7 @@ const Register: React.FC = () => {
             <Input
               id="confirmPassword"
               type="password"
-              {...register('confirmPassword')}
+              {...register("confirmPassword")}
             />
             {errors.confirmPassword && (
               <p className="error-message">{errors.confirmPassword.message}</p>
@@ -78,14 +97,14 @@ const Register: React.FC = () => {
           <div className="grid grid-cols-2 gap-4 mt-4">
             <div className="grid gap-2 ">
               <Label htmlFor="city">City</Label>
-              <Input id="city" {...register('city')} />
+              <Input id="city" {...register("city")} />
               {errors.city && (
                 <p className="error-message">{errors.city.message}</p>
               )}
             </div>
             <div className="grid gap-2">
               <Label htmlFor="address">Address</Label>
-              <Input id="address" {...register('address')} />
+              <Input id="address" {...register("address")} />
               {errors.address && (
                 <p className="error-message">{errors.address.message}</p>
               )}
