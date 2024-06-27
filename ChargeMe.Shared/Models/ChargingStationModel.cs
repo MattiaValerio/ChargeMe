2f8983e7b0cce0a6bb24b7a03417b184925207b7
Mattia Valerio <mattia.valerio@stud.itsaltoadriatico.it>
2024-06-27 10:12:27 +0200

    fix controllers

--- ChargeMe.Shared/Models/ChargingStationModel.cs
+++ ChargeMe.Shared/Models/ChargingStationModel.cs
@@ -7,15 +7,26 @@ public class ChargingStation
 {
     [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity), Key()]
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; }
     public ChargingStationStatus Status { get; set; }
-    public string City { get; set; } = string.Empty;
-    public string Address { get; set; } = string.Empty;
-    public string State { get; set; } = string.Empty;
-    public string ZipCode { get; set; } = string.Empty;
+    public string City { get; set; } 
+    public string Address { get; set; } 
+    public string State { get; set; } 
+    public string ZipCode { get; set; } 
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    public Car? ChargingCar { get; set; }
+
+    public ChargingStation(string name, ChargingStationStatus status, string city, string address, string state, string zipCode, double latitude, double longitude)
+    {
+        Name = name;
+        Status = status;
+        City = city;
+        Address = address;
+        State = state;
+        ZipCode = zipCode;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
 }
 
 public enum ChargingStationStatus
@@ -25,27 +36,3 @@ public enum ChargingStationStatus
     OutOfService = 2
 }
 
-public record GetChargingStation(
-    int Id,
-    string Name,
-    ChargingStationStatus Status,
-    string City,
-    string Address,
-    string State,
-    string ZipCode,
-    double Latitude,
-    double Longitude,
-    GetCarId? ChargingCar
-    );
-
-public record InsertChargingStation(
-    string Name,
-    ChargingStationStatus Status,
-    string City,
-    string Address,
-    string State,
-    string ZipCode,
-    double Latitude,
-    double Longitude,
-    Car? ChargingCarId
-    );
